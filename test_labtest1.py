ef96dc517e40af447ec67a2c382308f08c3b2420
May Phyu Phyu Thaw <MAYPHYU.22@ichat.sp.edu.sg>
2023-11-28 16:50:21 +0800

    Implement Part B for labtest1.py

--- test_labtest1.py
+++ test_labtest1.py
@@ -1,15 +1,26 @@
 import labtest1
 
+cvs_list = [
+    {"date" : "13.01.2022", "expense_item" : "rice", "price" : "13.5"},
+    {"date" : "15.01.2022", "expense_item" : "butter", "price" : "3.5"},
+    {"date" : "17.01.2022", "expense_item" : "sugar", "price" : "2.8"},
+    {"date" : "19.01.2022", "expense_item" : "apples", "price" : "4.7"},
+    {"date" : "20.01.2022", "expense_item" : "oranges", "price" : "3"},
+    {"date" : "22.01.2022", "expense_item" : "potatos", "price" : "2.5"},
+    {"date" : "30.01.2022", "expense_item" : "rice", "price" : "12"}
+]
 
 def test_calc_average_expenses():
-    csv_list=labtest1.load_csv_database()
+    result = []
+    avg_expense = labtest1.calc_average_expenses(cvs_list)
 
+    assert (avg_expense == (13.5 + 3.5 + 2.8 + 4.7 + 3 + 2.5 + 12)/ len(cvs_list))
 
 
 
 def test_calc_total_expenses():
-    csv_list=labtest1.load_csv_database()
-
+    #csv_list=labtest1.load_csv_database()
+    total_exp = labtest1.calc_total_expenses()
 
 def test_is_price_range_valid():
 
